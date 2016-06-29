0f2bf738ec2abea521781bca2b8fe72854dd35bc
avi-jain <avijain97@ymail.com>
2016-06-29 21:56:29 +0530

    first commit

---
+++ src/components/Email.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Email = React.createClass({
+  render: function(){
+    return(
+        <div>
+        <input type='email'>
+        </div>
+    );
+  }
+});
---
+++ src/components/Name.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Name = React.createClass({
+  render: function(){
+    return(
+        <div>
+        <input type='text'>
+        </div>
+    );
+  }
+});
